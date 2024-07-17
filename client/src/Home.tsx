2bd3176c94976ba0eb3d2d1a25245ba5ebb8aa50
gillancodes <gillancodes@laposte.net>
2024-07-17 23:06:37 +0200

    feat: translation sys done

--- client/src/Home.tsx
+++ client/src/Home.tsx
@@ -3,8 +3,8 @@ import { getTranslation } from './langs/translation'
 
 export default function Home() {
   return (
-    <div>
-        {getTranslation("fr_fr", "home", "welcome")}
+    <div className='test'>
+        {getTranslation("home", "welcome")}
     </div>
   )
 }
--- client/src/langs/translation.ts
+++ client/src/langs/translation.ts
@@ -1,6 +1,26 @@
-export function getTranslation (file:string, section:string, text:string)
+export function getTranslation (section:string, text:string)
 {
-    const translation = require(`./${file}.json`);
-    return translation[section][text]
+
+    const lang = getCookie("lang");
+    var translation;
+
+    if (!lang)
+    {
+        document.cookie = "lang=en_en";
+    } else {
+        try {
+            translation = require(`./${lang}.json`);
+        } catch (err:any) {
+            translation = require('./en_en.json');
+        }
+    }
+
+    if (translation)
+        return translation[section][text];
 }
 
+function getCookie(name:string) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts?.pop()?.split(';').shift();
+}
