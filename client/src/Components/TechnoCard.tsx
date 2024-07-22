37175fb34e317bbe89497ebc307dfadd885b6fb2
gillancodes <gillancodes@laposte.net>
2024-07-22 10:48:44 +0200

    refactor: card css name

--- client/src/Components/TechnoCard.tsx
+++ client/src/Components/TechnoCard.tsx
@@ -1,8 +1,7 @@
-import React from 'react'
 
 export default function TechnoCard({icon, name}: {icon:string, name:string}) {
   return (
-    <li className='card'>
+    <li className='tech-card'>
         <div id='icon'>
             <img src={`/icons/${icon}`} alt={name} />
         </div>
