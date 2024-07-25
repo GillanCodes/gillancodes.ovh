312151f9a449439e85680c6ffc24c47e864bf400
gillancodes <gillancodes@laposte.net>
2024-07-25 11:45:35 +0200

    feat: auth page done

--- client/src/Pages/Auth.tsx
+++ client/src/Pages/Auth.tsx
@@ -1,20 +1,52 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react'
 
 export default function Auth() {
-  return (
-    <main>
-        <form>
-            <div className="fields">
-                <div className="field">
-                    <label htmlFor="">Email</label>
-                    <input type="email" name="email" id="email" />
+
+    const [logs, setLogs] = useState({username: "", password: ""});
+
+    const submitHandle = (e:React.FormEvent) => {
+        e.preventDefault();
+
+        console.log(logs)
+
+        axios({
+            method:"POST",
+            withCredentials: true,
+            url: `${process.env.REACT_APP_API_URL}/auth/signin`,
+            data: {
+                username: logs.username,
+                password: logs.password
+            }
+        }).then((res) => {
+            // const win:Window = window;
+            // win.location = '/';
+            console.log(res);
+            console.log("logged !")
+        }).catch((err) => {
+            console.log(err);
+        });
+    }
+
+    return (
+        <main>
+            <form onSubmit={submitHandle}>
+                <div className="fields">
+                    <div className="field">
+                        <label htmlFor="">Username</label>
+                        <input type="text" name="username" id="username" onChange={(e:any) => setLogs({...logs, username:e.target.value})}/>
+                    </div>
+                    <div className="field">
+                        <label htmlFor="">Password</label>
+                        <input type="password" name="pswd" id="pswd" onChange={(e:any) => setLogs({...logs, password:e.target.value})}/>
+                    </div>
                 </div>
-                <div className="field">
-                    <label htmlFor="">Password</label>
-                    <input type="password" name="pswd" id="pswd" />
+                <div className="fields">
+                    <div className="field">
+                        <input type="submit" value="Login" />
+                    </div>
                 </div>
-            </div>
-        </form>
-    </main>
-  )
+            </form>
+        </main>
+    )
 }
