d7178d9f0f2d845502010611de0cae3c3f1cac18
gillancodes <gillancodes@laposte.net>
2024-07-26 11:29:24 +0200

    feat: added logout btn

--- client/src/Components/Navbar/Navbar.tsx
+++ client/src/Components/Navbar/Navbar.tsx
@@ -3,10 +3,24 @@ import LangBtn from "./LangBtn";
 import ThemeBtn from "./ThemeBtn";
 import { UIdContext } from "../../App.context";
 import { NavLink } from "react-router-dom";
+import axios from "axios";
 
 export default function Navbar() {
 
   const UId = useContext(UIdContext);
+
+  const logout = () => {
+    axios({
+      method: "post",
+      url: `${process.env.REACT_APP_API_URL}/auth/signout`,
+      withCredentials:true
+    }).then((res) => {
+      if (res)
+        window.location.reload();
+    }).catch((err) => {
+      return console.log(err);
+    })
+  }
 
   return (
     <nav className='main-nav'>
@@ -15,7 +29,10 @@ export default function Navbar() {
         </a>
         <div className="controls">
             {UId && (
-              <NavLink to={"/dashboard"} className={"nav-btn"}>D</NavLink>
+              <>
+                <p className="nav-btn" onClick={logout}>L</p>
+                <NavLink to={"/dashboard"} className={"nav-btn"}>D</NavLink>
+              </>
             )}
             <ThemeBtn />
             <LangBtn /> 
