7f131aec6972aa307ec4c9991b8de58a429385fc
gillancodes <gillancodes@laposte.net>
2024-07-26 10:50:17 +0200

    feat: added logo

--- client/src/Pages/Home.tsx
+++ client/src/Pages/Home.tsx
@@ -30,7 +30,7 @@ export default function Home() {
           <h1>Gillian <span className='highlight'>Gund</span>, <br /> {getTranslation("introduction", "work")}</h1>
           <p>{getTranslation("introduction", "description")} <span className='highlight'>ReactJs</span>, <span className='highlight'>NodeJs</span> & <span className='highlight'>MongoDB</span></p>
         </div>
-        <img src="" alt="Logo" />
+        <img src="/memo.png" alt="Logo" className='protrait' />
       </section>
       <section id='works'>
         <h2 className='title'>{getTranslation("work", "title")}</h2>
