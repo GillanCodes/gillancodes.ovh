59010c5ed2edc24638a3f374a3395ca6837e7232
gillancodes <gillancodes@laposte.net>
2024-12-06 14:22:57 +0100

    fix: update log

--- api/log.ts
+++ api/log.ts
@@ -1,6 +1,6 @@
-import * as fs from "fs";
-import config from "./config/config";
-import * as path from "path";
+import * as fs from "node:fs";
+import * as path from "node:path";
+import sanitizedConfig from "./config/config";
 
 //Init color vars
 const Reset = "\x1b[0m",
@@ -12,19 +12,21 @@ FgBlack = "\x1b[30m",
 BgGreen = "\x1b[42m"
 
 //init constant
-let appName:string = config.APP_NAME;
+const appName:string = sanitizedConfig.APP_NAME;
 const timestamp:Date = new Date();
 const time = (): string => { 
 	return new Date(new Date().valueOf() * 1).toLocaleDateString("fr-FR", {hour: "2-digit", minute: "2-digit", second:"2-digit", weekday: "long", year: "numeric", month: "short", day:"numeric"})
 }
 const fileName:string = `${timestamp.getDate()}_${timestamp.getMonth() + 1}_${timestamp.getFullYear()}.log`;
-const sanitizedPath = path.join(__dirname, config.LOG_FOLDER);
+const sanitizedPath = path.join(__dirname, sanitizedConfig.LOG_FOLDER);
 const filePath:string = `${sanitizedPath}${fileName}`;
 
 //Check if "logs" folder exist ?
-if (!fs.existsSync(sanitizedPath)){
-    fs.mkdirSync(sanitizedPath);
-}
+fs.readdir(sanitizedPath, (err , data) => {
+    if (err) fs.mkdir(sanitizedPath, (err:any) => {
+        if (err) throw Error(err);
+    });
+});
 
 //check if log file exist
 fs.readFile(filePath, (err, data) => {
@@ -34,42 +36,44 @@ fs.readFile(filePath, (err, data) => {
 });
 
 const log = (message:string|object|null|undefined|number, type?:string|number|undefined|null): void => {
+    let ln:string = "";
     switch(type)
     {
         case 0:
-        case "error":
-            var ln = `[${time()}] [ERROR] ${appName} - ${message}`;
+        case "info":
+            ln = `[${time()}] [INFO] ${appName} - ${message}`;
             fs.appendFileSync(filePath, `${ln}\n`);
-            console.log(FgRed, ln, Reset);
+            console.log(`${FgCyan}${ln}${Reset}`);
             break;
+
         case 1:
         case "success":
-            var ln = `[${time()}] [SUCCESS] ${appName} - ${message}`;
+            ln = `[${time()}] [SUCCESS] ${appName} - ${message}`;
             fs.appendFileSync(filePath, `${ln}\n`);
-            console.log(FgGreen, ln, Reset);
+            console.log(`${FgGreen}${ln}${Reset}`);
             break;
         case 2:
         case "warn":
-            var ln = `[${time()}] [WARN] ${appName} - ${message}`;
+            ln = `[${time()}] [WARN] ${appName} - ${message}`;
             fs.appendFileSync(filePath, `${ln}\n`);
-            console.log(FgYellow, ln, Reset);
+            console.log(`${FgYellow}${ln}${Reset}`);
             break;
         case 3:
-        case "info":
-            var ln =  `[${time()}] [INFO] ${appName} - ${message}`;
+        case "error":          
+            ln = `[${time()}] [ERROR] ${appName} - ${message}`;
             fs.appendFileSync(filePath, `${ln}\n`);
-            console.log(FgCyan, ln, Reset);
+            console.log(`${FgRed}${ln}${Reset}`);
             break;
         case 4:
         case "debug":
-            var ln =  `[${time()}] [DEBUG] ${appName} - ${message}`;
+            ln =  `[${time()}] [DEBUG] ${appName} - ${message}`;
             fs.appendFileSync(filePath, `${ln}\n`);
-            console.log(BgGreen + FgBlack, ln, Reset);
+            console.log(`${BgGreen}${FgBlack}${ln}${Reset}`);
             break;
         default:
-            var ln =  `[${time()}] [---] ${appName} - ${message}`;
+            ln =  `[${time()}] [---] ${appName} - ${message}`;
             fs.appendFileSync(filePath, `${ln}\n`);
-            console.log(Reset, ln, Reset);
+            console.log(`${FgCyan}${ln}${Reset}`);
             break;
     }
     return;
