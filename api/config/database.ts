e27322eae9824c56c9c30c298524d7f9e86cc4d2
gillancodes <gillancodes@laposte.net>
2024-12-09 14:29:58 +0100

    feat: edit db config

--- api/config/database.ts
+++ api/config/database.ts
@@ -5,8 +5,8 @@ import log from '../log';
 
 //Connection to DB
 connect(config.DB_CONNECT_STRING)
-    .then(() => {
-        log("MongoDB : Connected", "success");
-    }).catch((err: string) => {
-        log(err, "error");
-    });
+  .then(() => {
+    log("MongoDB : Connected", "success");
+  }).catch((err: string) => {
+    log(err, "error");
+  });
