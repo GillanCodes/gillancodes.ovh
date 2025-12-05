18abe5f01d1f1d25fe8f02b0dbe9331064ff49af
gillancodes <gillancodes@laposte.net>
2025-12-05 11:40:32 +0100

    feat: added tech controller

--- server/src/features/Tech/tech.routes.ts
+++ server/src/features/Tech/tech.routes.ts
@@ -1,6 +1,15 @@
 import { Router } from "express";
+import { createTech, deleteTech, editTech, getTech, getTechs } from "./tech.controller";
 let router:Router = Router()
 
-router.get('/');
+router.get('/', getTechs);
+router.get('/:id', getTech);
+
+router.post('/', createTech);
+
+router.patch('/:id', editTech);
+
+router.delete('/:id', deleteTech);
+
 
 export default router;
