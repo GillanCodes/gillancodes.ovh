c72c33f5f29e2cebba1179c3bf023d659779d6dd
gillancodes <gillancodes@laposte.net>
2025-12-16 11:25:54 +0100

    feat: updated and added role support to user

--- server/src/features/User/user.controller.ts
+++ server/src/features/User/user.controller.ts
@@ -1,66 +1,85 @@
-import { Request, Response } from "express"; 
+import { Request, Response } from "express";
 import userModel from "./user.model";
 import { isValidObjectId } from "mongoose";
 import { isEmpty } from "@this/common/utils/isEmpty";
 import { editUserErrors } from "./user.error";
 import fs from "fs";
 import path from "path";
+import roleModel from "../Role/role.model";
 
-export const getUserById = async (req:Request, res:Response): Promise<void> => {
-  const { id } = req.params;
-  if (!isValidObjectId(id)) res.send("Error: not valid id");
+export class UserController {
+        static async getUserById(req: Request, res: Response): Promise<void> {
+                const { id } = req.params;
+                if (!isValidObjectId(id)) res.send("Error: not valid id");
 
-  let user = await userModel.findById(id).select('-password').populate('role');
-  if (isEmpty(user)) res.send("Error: No user Found")
-  res.status(200).send(user);
-  
-}
+                let user = await userModel.findById(id).select('-password').populate('role');
+                if (isEmpty(user)) res.send("Error: No user Found")
+                res.status(200).send(user);
 
-export const editUser = async (req: Request, res:Response):Promise<void> => {
-  try {
-    const { username, role }: { username?:string, role?:string } = req.body;
-    const file = req.file;
+        }
 
-    const mimetype_whitlist = ["image/jpeg"];
+        static async editUser(req: Request, res: Response): Promise<void> {
+                try {
+                        const { username, role }: { username?: string, role?: string } = req.body;
+                        const file = req.file;
 
-    if (file && !mimetype_whitlist.includes(file.mimetype)) {
-      throw Error("wrong picture type");
-    }
+                        const mimetype_whitlist = ["image/jpeg"];
 
-    const user = await userModel.findById(res.locals.user._id);
-    if(!user) return void res.status(200).send("Error: No user Found");
+                        if (file && !mimetype_whitlist.includes(file.mimetype)) {
+                                throw Error("wrong picture type");
+                        }
 
-    if (username) user.username = username.toLocaleLowerCase().split(" ").join("_");
-    if (file) {
-      var filePath = path.join(__dirname, `../../../uploads/${user.avatar}`);
-      fs.unlink(filePath, (err:any) => {
-        throw new Error(err);
-      });
-      user.avatar = file.filename; 
-    }
+                        const user = await userModel.findById(res.locals.user._id);
+                        if (!user) return void res.status(200).send("Error: No user Found");
 
-    if(role) user.role = role;
- 
-    await user.save();
-    return void res.status(201).json(user);
-  } catch (error:any) {
-    const errors = editUserErrors(error);
-    console.log(errors);
-    
-    return void res.status(201).send({errors});
-  }
+                        if (username) user.username = username.toLocaleLowerCase().split(" ").join("_");
+                        if (file) {
+                                var filePath = path.join(__dirname, `../../../uploads/${user.avatar}`);
+                                fs.unlink(filePath, (err: any) => {
+                                        throw new Error(err);
+                                });
+                                user.avatar = file.filename;
+                        }
 
-}
+                        if (role) user.role = role;
 
-export const deleteUser = async (req:Request, res:Response): Promise<void> => {
-    
-  if (!res.locals.user) return void res.status(403).send("Unauthorized");
+                        await user.save();
+                        return void res.status(201).json(user);
+                } catch (error: any) {
+                        const errors = editUserErrors(error);
+                        console.log(errors);
 
-  const { password } : { password:string } = req.body;
+                        return void res.status(201).send({ errors });
+                }
 
-  let pswd = await userModel.checkPassword(password, res.locals.user._id);
-  if (!pswd) return void res.status(201).send("Error: Wrong password");
+        }
 
-  const user = await userModel.findByIdAndDelete(res.locals.user._id)    
-  return void res.status(201).json(user);
+        static async updateUserRole(req: Request, res: Response) {
+                const { userId, roleId } = req.body;
+                if (!isValidObjectId(userId) || !isValidObjectId(roleId)) return res.status(200).send({ error: "Invalid Ids" });
+
+                const user = await userModel.findById(userId);
+                if (isEmpty(user)) return res.status(200).send({ error: "user_not_found" });
+                
+                const role = await roleModel.findById(roleId);
+                if (isEmpty(role)) return res.status(200).send({ error: "role_not_found" });
+
+                user!.role = role!._id;
+                user!.save();
+
+                return res.status(201).send(user);
+        }
+
+        static async deleteUser(req: Request, res: Response): Promise<void> {
+
+                if (!res.locals.user) return void res.status(403).send("Unauthorized");
+
+                const { password }: { password: string } = req.body;
+
+                let pswd = await userModel.checkPassword(password, res.locals.user._id);
+                if (!pswd) return void res.status(201).send("Error: Wrong password");
+
+                const user = await userModel.findByIdAndDelete(res.locals.user._id)
+                return void res.status(201).json(user);
+        }
 }
--- server/src/features/User/user.routes.ts
+++ server/src/features/User/user.routes.ts
@@ -1,14 +1,17 @@
 import { Router } from "express";
 let router:Router = Router()
 
-import { deleteUser, editUser, getUserById } from "./user.controller";
 import { requireAuth } from "../../middlewares/auth.middleware";
 
 import { multerMiddleware } from "../../middlewares/multer.middleware";
+import { UserController } from "./user.controller";
+import { checkPermission } from "../../middlewares/checkPermission.middleware";
+import { Permission } from "../Permission/permission.types";
 
-router.patch('/', requireAuth, multerMiddleware, editUser);
-router.delete('/', requireAuth, deleteUser);
+router.patch('/', requireAuth, multerMiddleware, UserController.editUser);
+router.patch('/', requireAuth, checkPermission(Permission.ADMIN_CHANGE_USER_ROLE), UserController.updateUserRole);
+router.delete('/', requireAuth, UserController.deleteUser);
 
-router.get('/:id', getUserById);
+router.get('/:id', UserController.getUserById);
 
 export default router;
