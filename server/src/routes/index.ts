766d61d1d26b69556fd1f376f474f2ef5e024be9
gillancodes <gillancodes@laposte.net>
2025-12-16 11:26:02 +0100

    feat: added role routes

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -4,9 +4,11 @@ const router:Router = Router();
 import AuthRoutes from "../features/Auth/auth.routes";
 import UserRoutes from "../features/User/user.routes";
 import TechRoutes from "../features/Tech/tech.routes";
+import RoleRoutes from "../features/Role/role.routes";
 
 router.use('/auth', AuthRoutes);
 router.use('/user', UserRoutes);
+router.use('/role', RoleRoutes);
 
 router.use('/tech', TechRoutes);
 
